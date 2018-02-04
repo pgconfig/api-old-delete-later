6eabfb93d9f08e1b0f8b5fe2c8b5b1254b360049
Sebastian Webber <sebastian@swebber.me>
2018-02-04 14:57:58 -0200

    add utils

---
+++ params/util.go
@@ -0,0 +1,12 @@
+package params
+
+const (
+	// Byte is Defined in Bytes
+	Byte = 1 << (10 * iota)
+	// KyloByte is Defined in Bytes
+	KyloByte
+	// MegaByte is Defined in Bytes
+	MegaByte
+	// GigaByte is Defined in Bytes
+	GigaByte
+)
